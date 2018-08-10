0497567cc146d92853e8fa4a5f524555354c4592
Robert Langhammer <rlanghammer@web.de>
2018-08-10 23:21:22 +0200

    bridge_functions: adjust for fff
    generate random mac for the bridges
    
    Signed-off-by: Robert Langhammer <rlanghammer@web.de>

--- broker/scripts/bridge_functions.sh
+++ broker/scripts/bridge_functions.sh
@@ -1,21 +1,16 @@
-ensure_policy()
-{
-  ip rule del $*
-  ip rule add $*
-}
-
 ensure_bridge()
 {
   local brname="$1"
-  brctl addbr $brname 2>/dev/null
-  
+  brctl addbr $brname 2>/dev/null 
+
   if [[ "$?" == "0" ]]; then
     # Bridge did not exist before, we have to initialize it
+    # generate random MAC 
+    local mac=$(hexdump -n5 -e'"06" 5/1 ":%02X"' /dev/urandom)
+    ip link set dev $brname address $mac 
     ip link set dev $brname up
-    # TODO The IP address should probably not be hardcoded here?
-    ip addr add 10.254.0.2/16 dev $brname
-    # TODO Policy routing should probably not be hardcoded here?
-    ensure_policy from all iif $brname lookup mesh prio 1000
+    # add new bridge to bat-device
+    batctl -m $BATDEV if add $brname
     # Disable forwarding between bridge ports
     ebtables -A FORWARD --logical-in $brname -j DROP
   fi
